b997e64e62fd58b97c23a0836c641b08b86d963b
Wastalas <wastalasdassise@gmail.com>
2025-07-02 13:15:18 +0200

    updated  files

--- agent.py
+++ agent.py
@@ -1,12 +1,108 @@
 from llm import llm
 from graph import graph
 
-# Create a movie chat chain
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.tools import Tool
+from langchain_neo4j import Neo4jChatMessageHistory
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from tools.vector import get_student_error_feedback
+from utils import get_session_id
 
-# Create a set of tools
+# Math tutor chat prompt
+chat_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a tutor expert providing information about mathematics."),
+        ("human", "{input}"),
+    ]
+)
 
-# Create chat history callback
+math_chat = chat_prompt | llm | StrOutputParser()
 
-# Create the agent
+# Tools list
+tools = [
+    Tool.from_function(
+        name="General Chat",
+        description="For general mathematics chat not covered by other tools.",
+        func=math_chat.invoke,
+    ),
+    Tool.from_function(
+        name="Student Error Feedback",
+        description="Analyze and provide feedback on student errors using LLM explanations and data retrieved from the database.",
+        func=get_student_error_feedback,
+    )
+]
 
-# Create a handler to call the agent
+# Chat history callback
+def get_memory(session_id):
+    return Neo4jChatMessageHistory(session_id=session_id, graph=graph)
+
+# Agent prompt template
+agent_prompt = PromptTemplate.from_template("""
+You are a tutor expert providing information about mathematics.
+Be as helpful as possible and return as much information as possible.
+Do not answer any questions that do not relate to mathematics.
+
+Do not answer any questions using your pre-trained knowledge, only use the information provided in the context.
+
+TOOLS:
+------
+
+You have access to the following tools:
+
+{tools}
+
+To use a tool, please use the following format:
+
+
+```
+Thought: Do I need to use a tool? Yes
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+```
+
+When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
+
+```
+Thought: Do I need to use a tool? No
+Final Answer: [your response here]
+```
+
+Begin!
+
+Previous conversation history:
+{chat_history}
+
+New input: {input}
+{agent_scratchpad}
+""")
+
+# Agent and executor
+agent = create_react_agent(llm, tools, agent_prompt)
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=tools,
+    verbose=True
+)
+
+chat_agent = RunnableWithMessageHistory(
+    agent_executor,
+    get_memory,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+)
+
+def generate_response(user_input):
+    """
+    Handler to call the Conversational agent and return a response for the UI.
+    """
+    try:
+        response = chat_agent.invoke(
+            {"input": user_input},
+            {"configurable": {"session_id": get_session_id()}},
+        )
+        return response['output']
+    except Exception as e:
+        return f"Sorry, an error occurred: {e}"
---
+++ tools/vector.py
@@ -0,0 +1,67 @@
+import streamlit as st
+from llm import llm, embeddings
+from graph import graph
+
+# tag::import_vector[]
+from langchain_neo4j import Neo4jVector
+# end::import_vector[]
+# tag::import_chain[]
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+# end::import_chain[]
+
+# tag::import_chat_prompt[]
+from langchain_core.prompts import ChatPromptTemplate
+# end::import_chat_prompt[]
+
+# Create the Neo4jVector
+
+neo4jvector = Neo4jVector.from_existing_index(
+    embeddings,                              # (1)
+    graph=graph,                             # (2)
+    index_name="student_embeddding_index",                 # (3)
+    node_label="Student",                      # (4)
+    text_node_property="llm_response",               # (5)
+    embedding_node_property="student_error_embedding", # (6)
+    retrieval_query="""
+RETURN
+    node.llm_response AS text,
+    score,
+    {
+        student_id: node.student_id,
+        grade: [ (node)-[:ANSWERED]->(Answer)-[:FOR_QUESTION]->(Question) | Answer.grade ],
+        student_response: [ (node)-[:ANSWERED]->(Answer)-[:FOR_QUESTION]->(Question) | [node.student_id, Answer.response] ],
+        original_answer: [ (node)-[:ANSWERED]->(Answer)-[:FOR_QUESTION]->(Question) | [node.student_id, Answer.right_answer] ],
+        question_id: [ (node)-[:ANSWERED]->(Answer)-[:FOR_QUESTION]->(Question) | [node.student_id, Question.question_id] ]
+    } AS metadata
+"""
+)
+
+# Create the retriever
+retriever = neo4jvector.as_retriever()
+
+# Create the prompt
+instructions = (
+    "Use the given context to answer the question."
+    "If you don't know the answer, say you don't know."
+    "Context: {context}"
+)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", instructions),
+        ("human", "{input}"),
+    ]
+)
+
+# Create the chain 
+question_answer_chain = create_stuff_documents_chain(llm, prompt)
+error_retriever = create_retrieval_chain(
+    retriever, 
+    question_answer_chain
+)
+
+
+# Create a function to call the chain
+def get_student_error_feedback(input):
+    return error_retriever.invoke({"input": input})
